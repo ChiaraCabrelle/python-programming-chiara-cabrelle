324328852f41912022321ac60adf755bcbada51b
ChiaraCabrelle <45198910+ChiaraCabrelle@users.noreply.github.com>
2018-11-27 15:57:58 +0100

    second-commit

--- cell-volume.py
+++ cell-volume.py
@@ -1,7 +1,7 @@
 #This script calculation the volume of a sphere and print it to the screen
 from math import pi
 d = 10.0
-d = float(d/2)
-V = (4.0/3.0)*pi*(d**3)
-print("Volume of the sphere:%s "%d)
+r = d/2.0
+V = (4.0/3.0)*pi*(r**3)
+print(V)
 
