024bdc5bacdcdaf699f8e1b6741ec90dccd55d71
ChiaraCabrelle <45198910+ChiaraCabrelle@users.noreply.github.com>
2018-11-27 15:54:11 +0100

    fisrt-commit

---
+++ cell-volume.py
@@ -0,0 +1,7 @@
+#This script calculation the volume of a sphere and print it to the screen
+from math import pi
+d = 10.0
+d = float(d/2)
+V = (4.0/3.0)*pi*(d**3)
+print("Volume of the sphere:%s "%d)
+
